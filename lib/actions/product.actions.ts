6b1b4c0a5ce0f7b4d14c047fd6760f736043ba60
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-01-27 15:30:39 -0300

    chore: added Neon, Prisma & WS config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,11 @@
 'use server';
 
+import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
 import { convertToPlainObject } from '@/lib/utils';
-import { PrismaClient } from '@prisma/client';
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
