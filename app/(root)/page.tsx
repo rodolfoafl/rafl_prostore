dc3603bdbd49cc472cb471cbf1056474e645ab0e
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-01-27 15:02:46 -0300

    feat: added Product Action to fetch latest products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,13 @@
 import ProductList from '@/components/shared/product/list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
-import sampleData from '@/db/sample-data';
-
-export default function HomePage() {
-  const { products } = sampleData;
+export default async function HomePage() {
+  const latestProducts = await getLatestProducts()
 
   return (
     <>
       <ProductList
-        data={products}
+        data={latestProducts}
         title="Newest Arrivals"
         limit={4}
       />
