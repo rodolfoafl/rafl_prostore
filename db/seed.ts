ce4dfc143dea8d720871f2666061422c4555b6e2
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-01-28 02:17:30 -0300

    chore: added Users to sample data & updated seed script

--- db/seed.ts
+++ db/seed.ts
@@ -1,16 +1,22 @@
-import { Prisma, PrismaClient } from '@prisma/client';
-import sampleData from '@/db/sample-data';
+import { PrismaClient } from '@prisma/client'
+
+import sampleData from '@/db/sample-data'
 
 async function main() {
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient()
 
-  await prisma.product.deleteMany();
+  await prisma.product.deleteMany()
+  await prisma.account.deleteMany()
+  await prisma.session.deleteMany()
+  await prisma.verificationToken.deleteMany()
+  await prisma.user.deleteMany()
 
-  await prisma.product.createMany({ data: sampleData.products });
+  await prisma.product.createMany({ data: sampleData.products })
+  await prisma.user.createMany({ data: sampleData.users })
 
-  console.log('Database seeded successfully!');
+  console.log('Database seeded successfully!')
 }
 
-main();
+main()
 
 // To run: npx tsx ./db/seed
