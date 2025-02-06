9460085a43f8d4c229ef59738eb7093cf6de0b2c
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-02-06 18:26:35 -0300

    chore: added utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -59,3 +59,52 @@ export function formatCurrency(amount: number | string | null) {
     return 'NaN'
   }
 }
+
+export function shortenId(id: string) {
+  return `..${id.substring(id.length - 6)}`
+}
+
+export function formatDateTime(dateString: Date) {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric',
+  }
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateTimeOptions,
+  )
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateOptions,
+  )
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    timeOptions,
+  )
+
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  }
+}
