81b5c9456a9e91863be38003ef1363cbdbac9791
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-02-07 01:03:13 -0300

    chore: added setup for Jest tests

---
+++ lib/paypal.ts
@@ -0,0 +1,29 @@
+const BASE = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com'
+
+export const paypal = {}
+
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    'base64',
+  )
+
+  const response = await fetch(`${BASE}/v1/oauth2/token`, {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+      Authorization: `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+
+  if (response.ok) {
+    const jsonData = await response.json()
+    return jsonData.access_token
+  } else {
+    const errorMessage = await response.text()
+    throw new Error(errorMessage)
+  }
+}
+
+export { generateAccessToken }
